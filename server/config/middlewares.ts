7af5d5c8f2b911811ed54df517f651d4cff9b973
Rafi Dirgantara <rafi.dirgan@gmail.com>
2025-04-26 23:58:48 +0700

    add cors

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -3,6 +3,16 @@ export default [
   "strapi::errors",
   "strapi::security",
   "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true, // enable CORS
+      origin: ["http://localhost:3000", "https://seger.rafidirg.com"], // allowed origins
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: "*", // allow all headers
+      credentials: true, // if you need to send cookies or auth headers
+    },
+  },
   "strapi::query",
   "strapi::body",
   "strapi::session",
