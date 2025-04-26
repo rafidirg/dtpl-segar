d7bbbf3a61a916cb751213fd757c87d068342a18
Rafi Dirgantara <rafi.dirgan@gmail.com>
2025-04-26 23:06:24 +0700

    fix typo

--- client/src/utils/get-strapi-url.ts
+++ client/src/utils/get-strapi-url.ts
@@ -1,5 +1,5 @@
 export function getStrapiURL() {
-  return process.env.STRPI_API_URL ?? "http://localhost:1337";
+  return process.env.STRAPI_API_URL ?? "http://localhost:1337";
 }
 
 export function getStrapiMedia(url: string) {
