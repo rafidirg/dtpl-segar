15faa598e7e2fccbf233bc627274c10752a2bb2f
Rafi Dirgantara <rafi.dirgan@gmail.com>
2025-04-26 23:52:19 +0700

    disable cors

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -2,15 +2,6 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: ["http://localhost:3000", "http://seger.rafidirg.com"], // Allow your frontend URL here
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      headers: "*",
-    },
-  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
